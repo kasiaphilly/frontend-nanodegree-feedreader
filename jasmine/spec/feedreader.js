a31400b8514af9f2d1b2c7d67c5ed97a6e275b66
Katarzyna Elliott-Maksymowicz <maksymowicz.katarzyna@gmail.com>
2018-09-01 14:00:01 +0200

    minor stylistic changes

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,7 +3,6 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against the application.
  */
-
 /* We"re placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don"t run until the DOM is ready.
@@ -22,24 +21,24 @@ $(function() {
 
         /* This test loops through each feed in the allFeeds object and
          * ensures it has a URL defined and that the URL is not empty. */
-         it("should have a URL", function() {
-           allFeeds.forEach(function(feed){
-             expect(feed.url).toBeDefined();
-             expect(feed.url.length).not.toBe(0);
-           });
-         });
+        it("should have a URL", function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
 
         /* This test loops through each feed in the allFeeds object and
          * ensures it has a name defined and that the name is not empty. */
         it("should have a name", function() {
-           allFeeds.forEach(function(feed){
-             expect(feed.name).toBeDefined();
-             expect(feed.name.length).not.toBe(0);
-           });
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
         });
     });
 
-/* This test suite tests the visibility of the menu*/
+    /* This test suite tests the visibility of the menu*/
     describe("The menu", function() {
 
         const body = document.body;
@@ -50,8 +49,8 @@ $(function() {
             expect(body.className).toContain("menu-hidden");
         });
 
-         /* This test ensures that the menu changes visibility when
-          * the menu icon is clicked. */
+        /* This test ensures that the menu changes visibility when
+         * the menu icon is clicked. */
         it("hides and shows on click", function() {
             $(menu).trigger("click"); // first click on menu icon
             expect(body.className).not.toContain("menu-hidden");
@@ -64,39 +63,39 @@ $(function() {
     /* This test suite checks if the feed displays initial entries */
     describe("Initial Entries", function() {
 
-      beforeEach(function(done) {
-          loadFeed(0, done);
-      });
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
-      /* This test ensures that when the loadFeed function is called
-       * and completes its work, there is at least a single .entry element
-       * within the .feed container. */
+        /* This test ensures that when the loadFeed function is called
+         * and completes its work, there is at least a single .entry element
+         * within the .feed container. */
 
-      it("feed loads at least one entry", function(){
-        expect($(".entry").length).not.toBe(0);
-      });
+        it("feed loads at least one entry", function() {
+            expect($(".entry").length).not.toBe(0);
+        });
     });
 
     /* This test suite tests the mechanism of loading a new feed */
     describe("New Feed Selection", function() {
-      let previousFeed;
+        let previousFeed;
 
-      beforeEach(function(done) {
-        loadFeed(0, function() {
-          previousFeed = document.querySelector(".feed").innerHTML;
-          loadFeed(1, function() {
-            done();
-          });
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                previousFeed = document.querySelector(".feed").innerHTML;
+                loadFeed(1, function() {
+                    done();
+                });
+            });
         });
-      });
 
-      /* This test ensures that when a new feed is loaded
-       * by the loadFeed function, the content actually changes. */
-      const newFeed = document.querySelector(".feed").innerHTML;
+        /* This test ensures that when a new feed is loaded
+         * by the loadFeed function, the content actually changes. */
+        const newFeed = document.querySelector(".feed").innerHTML;
 
-      it("content changes after reloading feed", function(){
-        expect(newFeed).not.toEqual(previousFeed);
-      });
+        it("content changes after reloading feed", function() {
+            expect(newFeed).not.toEqual(previousFeed);
+        });
     });
 
 }());

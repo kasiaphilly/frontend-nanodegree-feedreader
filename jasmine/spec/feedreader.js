3ca0de153da3998b1d77687524d98d436e4dc2e3
Katarzyna <37025592+kasiaphilly@users.noreply.github.com>
2018-09-01 12:44:15 +0200

    implements RSS Feeds test suite
    this includes 3 tests: if allFeeds is defined, if each feed has a url and if each feed has a name.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,42 +1,43 @@
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests that will be run against the application.
  */
 
-/* We're placing all of our tests within the $() function,
+/* We"re placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
+ * to ensure they don"t run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-        it('are defined', function() {
+    /* This test suite tests if RSS feed is defined in the allFeeds
+    variable in the application. */
+
+    describe("RSS Feeds", function() {
+        /* This test makes sure that the allFeeds variable has been
+         * defined and that it is not empty. */
+        it("are defined", function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /* This test loops through each feed in the allFeeds object and
+         * ensures it has a URL defined and that the URL is not empty. */
+         it("should have a URL", function() {
+           allFeeds.forEach(function(feed){
+             expect(feed.url).toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+           });
+         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        /* This test loops through each feed in the allFeeds object and
+         * ensures it has a name defined and that the name is not empty. */
+        it("should have a name", function() {
+           allFeeds.forEach(function(feed){
+             expect(feed.name).toBeDefined();
+             expect(feed.name.length).not.toBe(0);
+           });
+        });
     });
 
 
